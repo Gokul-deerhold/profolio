733689166b461ebc9daeb57ad7a0d1b26f159375
PARIJAT\parijat <gokul@parijat.asia>
2024-04-23 20:02:56 +0545

    ading more content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,7 @@
-/**
-* Template Name: iPortfolio
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Updated: Mar 17 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 
 (function() {
   "use strict";
-
-  /**
-   * Easy selector helper function
-   */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -22,7 +12,7 @@
   }
 
   /**
-   * Easy event listener function
+  
    */
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
